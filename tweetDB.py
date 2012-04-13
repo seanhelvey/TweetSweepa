00958217b2e6f8b60f6b4def8c23b9c02a2f8923
Sean Helvey <smh436@nyu.edu>
2012-04-12 20:25:35 -0400

    added text boxes and redirect to results page

--- tweetDB.py
+++ tweetDB.py
@@ -2,6 +2,4 @@ from google.appengine.ext import db
 
 class Hack(db.Model):
     textBox1 = db.StringProperty()
-
-class Fix(db.Model):
-    textBox2 = db.StringProperty()
+    newTag = db.StringProperty()
